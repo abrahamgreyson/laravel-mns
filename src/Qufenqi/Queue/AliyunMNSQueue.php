ae5f70cc5a8dabb42c6132e77e326e0fc21313db
chefxu <rssidea@qq.com>
2016-04-20 22:37:07 +0800

    修复代码缩进问题

--- src/Qufenqi/Queue/AliyunMNSQueue.php
+++ src/Qufenqi/Queue/AliyunMNSQueue.php
@@ -5,7 +5,8 @@ use Illuminate\Contracts\Queue\Queue as QueueContract;
 use AliMNS\Client;
 use Config;
 
-class AliyunMNSQueue extends Queue implements QueueContract {
+class AliyunMNSQueue extends Queue implements QueueContract
+{
 
     private $client;
     private $queueMap;
@@ -17,59 +18,59 @@ class AliyunMNSQueue extends Queue implements QueueContract {
         $this->queueMap = $config['queue'];
     }
 
-	/**
-	 * Push a new job onto the queue.
-	 *
-	 * @param  string  $job
-	 * @param  mixed   $data
-	 * @param  string  $queue
-	 * @return mixed
-	 */
-	public function push($job, $data = '', $queue = null)
-	{
+    /**
+     * Push a new job onto the queue.
+     *
+     * @param  string  $job
+     * @param  mixed   $data
+     * @param  string  $queue
+     * @return mixed
+     */
+    public function push($job, $data = '', $queue = null)
+    {
         $payload = $this->createPayload($job, $data);
         return $this->pushRaw($payload, $queue);
-	}
+    }
 
-	/**
-	 * Push a raw payload onto the queue.
-	 *
-	 * @param  string  $payload
-	 * @param  string  $queue
-	 * @param  array   $options
-	 * @return mixed
-	 */
-	public function pushRaw($payload, $queue = null, array $options = array())
-	{
+    /**
+     * Push a raw payload onto the queue.
+     *
+     * @param  string  $payload
+     * @param  string  $queue
+     * @param  array   $options
+     * @return mixed
+     */
+    public function pushRaw($payload, $queue = null, array $options = array())
+    {
         $this->client->setQueue($this->getQueue($queue));
         return $this->client->publish($payload);
-	}
+    }
 
-	/**
-	 * Push a new job onto the queue after a delay.
-	 *
-	 * @param  \DateTime|int  $delay
-	 * @param  string  $job
-	 * @param  mixed   $data
-	 * @param  string  $queue
-	 * @return mixed
-	 */
-	public function later($delay, $job, $data = '', $queue = null)
-	{
+    /**
+     * Push a new job onto the queue after a delay.
+     *
+     * @param  \DateTime|int  $delay
+     * @param  string  $job
+     * @param  mixed   $data
+     * @param  string  $queue
+     * @return mixed
+     */
+    public function later($delay, $job, $data = '', $queue = null)
+    {
         $seconds = $this->getSeconds($delay);
         $payload = $this->createPayload($job, $data);
         $this->client->setQueue($this->getQueue($queue));
 
         return $this->client->publish($payload, $seconds);
-	}
+    }
 
-	/**
-	 * Pop the next job off of the queue.
-	 *
-	 * @param  string  $queue
-	 * @return \Illuminate\Queue\Jobs\Job|null
-	 */
-	public function pop($queue = null)
+    /**
+     * Pop the next job off of the queue.
+     *
+     * @param  string  $queue
+     * @return \Illuminate\Queue\Jobs\Job|null
+     */
+    public function pop($queue = null)
     {
         $this->client->setQueue($this->getQueue($queue));
         $job = $this->client->consume();
@@ -78,10 +79,10 @@ class AliyunMNSQueue extends Queue implements QueueContract {
         }
     }
 
-	protected function resolveJob($job)
-	{
-		return new Jobs\AliyunMNSJob($this->container, $job, $this->client);
-	}
+    protected function resolveJob($job)
+    {
+        return new Jobs\AliyunMNSJob($this->container, $job, $this->client);
+    }
 
     public function getQueue($queue = null)
     {
