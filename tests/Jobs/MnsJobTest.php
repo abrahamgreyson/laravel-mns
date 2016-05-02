341e5d3c0972ec55e162b60f37ae8d0713c49e7e
abrahamgreyson <82011220@qq.com>
2016-05-02 19:25:27 +0000

    简化命名空间，README，格式化 tests 文件。

--- tests/Jobs/MnsJobTest.php
+++ tests/Jobs/MnsJobTest.php
@@ -1,5 +1,17 @@
 <?php
 
+/*
+ * Laravel-Mns -- 阿里云消息队列（MNS）的 Laravel 适配。
+ *
+ * This file is part of the abe/laravel-mns.
+ *
+ * (c) Abraham Greyson <82011220@qq.com>
+ * @link: https://github.com/abrahamgreyson/laravel-mns
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace LaravelMns\Test;
 
 use AliyunMNS\Responses\ReceiveMessageResponse;
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,15 @@
 <?php
 
 /*
- * CloudAtlas
- * @link  : https://github.com/AbrahamGreyson/cloudatlas
- * @author: AbrahamGreyson <82011220@qq.com>
- * @license: MIT
+ * Laravel-Mns -- 阿里云消息队列（MNS）的 Laravel 适配。
+ *
+ * This file is part of the abe/laravel-mns.
+ *
+ * (c) Abraham Greyson <82011220@qq.com>
+ * @link: https://github.com/abrahamgreyson/laravel-mns
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
  */
 
 require __DIR__ . '/../vendor/autoload.php';
